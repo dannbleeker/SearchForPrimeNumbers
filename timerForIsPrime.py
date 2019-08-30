4bc0f5e41897552fd947b63c1fa3691f98e059a6
Dann Bleeker Pedersen <54210885+dannbleeker@users.noreply.github.com>
2019-08-30 07:11:10 +0200

    Update timerForIsPrime.py
    Timer for testing prime 5 with and without while loop added.

--- timerForIsPrime.py
+++ timerForIsPrime.py
@@ -6,30 +6,51 @@ import timeit
 TEST_CODE = '''
 import isPrimeSearch
 primeCounter = 0
-for currentNumber in range(100000, 200000):
+for currentNumber in range(1, 1000000):
     if isPrimeSearch.checkForPrime(currentNumber) == True:
         primeCounter += 1     
 '''
-print( "\nCheckForPrime1: %s" % timeit.timeit(stmt = TEST_CODE, number = 1)  )
+#print( "\nCheckForPrime1: %s" % timeit.timeit(stmt = TEST_CODE, number = 5)  )
 
 TEST_CODE = '''
 import isPrimeSearch
 primeCounter = 0
-for currentNumber in range(100000, 200000):
+currentNumber = 100000000
+while currentNumber <= 110000000:
+    if isPrimeSearch.checkForPrime5(currentNumber) == True:
+        primeCounter +=  1
+        currentNumber += 1
+    currentNumber += 1
+'''
+print( "\nCheckForPrime5 + 1: %s" % timeit.timeit(stmt = TEST_CODE, number = 5)  )
+
+TEST_CODE = '''
+import isPrimeSearch
+primeCounter = 0
+for currentNumber in range(100000000, 110000000):
+    if isPrimeSearch.checkForPrime5(currentNumber) == True:
+        primeCounter += 1     
+'''
+print( "\nCheckForPrime5: %s" % timeit.timeit(stmt = TEST_CODE, number = 5)  )
+
+TEST_CODE = '''
+import isPrimeSearch
+primeCounter = 0
+for currentNumber in range(1000000, 2000000):
     if isPrimeSearch.checkForPrime2(currentNumber) == True:
         primeCounter += 1     
 '''
-print( "CheckForPrime2: %s" % timeit.timeit(stmt = TEST_CODE, number = 1)  )
+#print( "CheckForPrime2: %s" % timeit.timeit(stmt = TEST_CODE, number = 1)  )
 
 
 TEST_CODE = '''
 import isPrimeSearch
 primeCounter = 0
-for currentNumber in range(100000, 200000):
+for currentNumber in range(1000000, 2000000):
     if isPrimeSearch.checkForPrime3(currentNumber) == True:
         primeCounter += 1     
 '''
-print( "CheckForPrime3: %s" % timeit.timeit(stmt = TEST_CODE, number = 1)  )
+#print( "CheckForPrime3: %s" % timeit.timeit(stmt = TEST_CODE, number = 1)  )
 
 
 TEST_CODE = '''
@@ -39,5 +60,5 @@ for currentNumber in range(100000, 200000):
     if isPrimeSearch.checkForPrime4(currentNumber) == True:
         primeCounter += 1     
 '''
-print( "CheckForPrime4: %s" % timeit.timeit(stmt = TEST_CODE, number = 1)  )
+#print( "CheckForPrime4: %s" % timeit.timeit(stmt = TEST_CODE, number = 1)  )
 

75a2b2b7ed7d047b0fc2a183b6924de284b01a72
Dann Bleeker Pedersen <54210885+dannbleeker@users.noreply.github.com>
2019-09-01 13:33:24 +0200

    Update timerForIsPrime.py
    while loop with skip ahead in an if else statement is faster (0,4%) than without else. Plain version deleted.

--- timerForIsPrime.py
+++ timerForIsPrime.py
@@ -9,36 +9,16 @@ if (useLogFile):
 # Indsæt variable heri
 
 startNumber = 0
-endNumber =   20000000
+endNumber = 100000000
 
 print("Searching from %s to %s (interval: %s)" % (
-f"{startNumber:,d}".replace(",", "."), f"{endNumber:,d}".replace(",", "."),
-f"{(endNumber - startNumber):,d}".replace(",", ".")))
+    f"{startNumber:,d}".replace(",", "."), f"{endNumber:,d}".replace(",", "."),
+    f"{(endNumber - startNumber):,d}".replace(",", ".")))
 
 if (useLogFile):
     print("Searching from %s to %s (interval: %s)" % (
-    f"{startNumber:,d}".replace(",", "."), f"{endNumber:,d}".replace(",", "."),
-    f"{(endNumber - startNumber):,d}".replace(",", ".")), file=logfile)
-
-## With skip one after prime - plain version
-
-for currentPrimeSearch in range(6, 7):
-    TEST_CODE = '''
-import isPrimeSearch
-primeCounter = 0
-currentNumber = %s
-while currentNumber <= %s:
-    if isPrimeSearch.checkForPrime%s(currentNumber) == True:
-        primeCounter +=  1
-        currentNumber += 1        
-    currentNumber += 1
-''' % (startNumber, endNumber, currentPrimeSearch)
-
-    print("CheckForPrime%s (wsa1): %s" % (currentPrimeSearch, round(timeit.timeit(stmt=TEST_CODE, number=1), 4)))
-    if (useLogFile):
-        print("CheckForPrime%s (wsa1): %s" % (currentPrimeSearch, round(timeit.timeit(stmt=TEST_CODE, number=1), 4)),
-              file=logfile)
-
+        f"{startNumber:,d}".replace(",", "."), f"{endNumber:,d}".replace(",", "."),
+        f"{(endNumber - startNumber):,d}".replace(",", ".")), file=logfile)
 
 ## With skip one after prime - revised version
 
@@ -59,7 +39,6 @@ while currentNumber <= %s:
     if (useLogFile):
         print("CheckForPrime%s (wsa2): %s" % (currentPrimeSearch, round(timeit.timeit(stmt=TEST_CODE, number=1), 4)),
               file=logfile)
-
 
 ## With for loop
 for currentPrimeSearch in range(6, 7):
